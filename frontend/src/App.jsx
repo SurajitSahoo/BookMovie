2cece88fb6b9b36d2d1c62c5587aa64ab2494319
Surajit Sahoo <surajitsahoo200@gmail.com>
2026-03-06 17:49:37 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import navbar from '../Pages/navbar';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <navbar/>
+      
+  )
+}
+
+export default App
---
+++ frontend/src/Pages/navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const navbar =()=>
+{
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+export default navbar;
